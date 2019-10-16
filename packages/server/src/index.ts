ff606c5e88cd9cc08a51e7a8fd229f933b53605c
Chris Zempel <chris.m.zempel@gmail.com>
2019-10-16 07:24:18 -0500

    refactor: makes koa responsive to deployment config

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import Koa from "koa";
 import * as graphql from "./server/graphql"
+
+const PORT = process.env.PORT || 3001;
 
 const app = new Koa();
 
 graphql.init(app)
 
-app.listen(3001);
+app.listen(PORT);
